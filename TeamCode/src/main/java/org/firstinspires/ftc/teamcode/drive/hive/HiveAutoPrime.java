e260351b370ffae6ab63cec6fba83a50e277a51e
abiIsCoolll <151551238+abiIsCoolll@users.noreply.github.com>
2023-11-20 12:29:19 -0600

    Update HiveAutoPrime.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/hive/HiveAutoPrime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/hive/HiveAutoPrime.java
@@ -247,7 +247,7 @@ public class HiveAutoPrime extends LinearOpMode {
             sleep(20000);
         }*/
         //MIDDLE
-//        encoderDrive(driveSpeed,24,5.0,0,0,0,5.0);
+//        encoderDrive(driveSpeed,24,5,0,0,1,5.0);
 //        encoderDrive(driveSpeed,0,10,1,0,1,5.0);
 //        encoderSpin(turnSpeed, 90,10,0,0,0,5.0);
 //        encoderDrive(driveSpeed,-38,15.0,0,0,5.0);
@@ -257,15 +257,28 @@ public class HiveAutoPrime extends LinearOpMode {
         
 
         //LEFT
-          encoderDrive(driveSpeed,28,5,0,0,0,5.0);
-          encoderSpin(turnSpeed,-90,10,0,0,0,5.0);
-          encoderDrive(driveSpeed,0,10,1,0,0,5.0);
-          
+          encoderDrive(driveSpeed,30,5,0,0,1,5.0);
+          encoderSpin(turnSpeed,-90,10,0,0,1,5.0);
+          encoderDrive(driveSpeed,0,10,1,0,1,5.0);
+          encoderStrafe(driveSpeed,-24,15,0,0,0,5.0);
+          encoderDrive(driveSpeed,38,15,0,0,0,5.0);
+          encoderStrafe(driveSpeed,24,15,0,0,0,5.0);
+          score(1,5.0);
+          encoderStrafe(driveSpeed,24,0,0,0,0,5.0);
+          encoderDrive(driveSpeed,8,0,0,0,0,5.0);
+                       
          
 
         //RIGHT
-
-
+          encoderDrive(driveSpeed,30,5,0,0,1,5.0);
+          encoderSpin(turnSpeed,90,10,0,0,1,5.0);
+          encoderDrive(driveSpeed,0,10,1,0,1,5.0);
+          encoderDrive(driveSpeed,-38,10,0,0,0,5.0);
+          score(1,5.0);
+          encoderStrafe(driveSpeed,24,0,0,0,0,5.0);
+          encoderDrive(driveSpeed,8,0,0,0,0,5.0);
+          
+  
 
 
         telemetry.addData("Path", "Complete");
