15921a3455425924162a2cd403c53b6cbc4ad8ed
DrueDude <114957476+DrueDude@users.noreply.github.com>
2023-11-25 12:15:19 -0600

    Update SwarmAutoBlueBack.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/swarm/SwarmAutoBlueBack.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/swarm/SwarmAutoBlueBack.java
@@ -232,8 +232,9 @@ public class SwarmAutoBlueBack extends LinearOpMode {
         */
 
         //MIDDLE
-        /*encoderDrive(driveSpeed,28,10,0,0,5.0);
-        encoderDrive(driveSpeed,0,10,-1,0,5.0);
+        /*encoderStrafe(driveSpeed,2,10,0,0,5.0);
+        encoderDrive(driveSpeed,28,10,0,0,5.0);
+        encoderIntake(-1,5.0);
         encoderSpin(turnSpeed,90,10,-1,0,5.0);
         encoderDrive(driveSpeed, -36,10,0, 0, 5.0);
         score(1,5.0);
@@ -242,25 +243,27 @@ public class SwarmAutoBlueBack extends LinearOpMode {
         */
         
         //LEFT
-        /*encoderDrive(driveSpeed,26,10,0,0,5.0);
+        /*encoderStrafe(driveSpeed,3,10,0,0,5.0);
+        encoderDrive(driveSpeed,26,10,0,0,5.0);
         encoderSpin(turnSpeed,-90,10,0,0,5.0);
         encoderDrive(driveSpeed, 2,10,0,0,5.0);
-        encoderDrive(driveSpeed,0,10,-1,0,5.0);
+        encoderIntake(-1,5.0);
         encoderSpin(turnSpeed,180,10,0,0,5.0);
         encoderStrafe(driveSpeed,-24,10,0,0,5.0);
         encoderDrive(driveSpeed, -36,10,0, 0, 5.0);
-        encoderStrafe(driveSpeed,24,10,0,0,5.0);
+        encoderStrafe(driveSpeed,30,10,0,0,5.0);
         score(1,5.0);
         encoderStrafe(driveSpeed,-24,0,0,0,5.0);
         encoderDrive(driveSpeed, -12,0,0,0,5.0);
         */
 
         //RIGHT
+        encoderStrafe(driveSpeed,1,10,0,0,5.0);
         encoderDrive(driveSpeed,26,10,0,0,5.0);
         encoderSpin(turnSpeed,90,10,0,0,5.0);
-        encoderDrive(driveSpeed, 2,10,0,0,5.0);
-        encoderDrive(driveSpeed,0,10,-1,0,5.0);
-        encoderDrive(driveSpeed, -36,10,0, 0, 5.0);
+        encoderIntake(-1,5.0);
+        encoderDrive(driveSpeed, -38,10,0, 0, 5.0);
+        encoderStrafe(driveSpeed,-6,10,0,0,5.0);
         score(1,5.0);
         encoderStrafe(driveSpeed,-24,0,0,0,5.0);
         encoderDrive(driveSpeed, -12,0,0,0,5.0);
