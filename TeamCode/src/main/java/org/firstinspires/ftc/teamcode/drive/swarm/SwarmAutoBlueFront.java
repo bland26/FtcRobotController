bdf6a034136f2b08b9e7af85f6ad6ab1419ce89c
bland26 <bland26@gmail.com>
2023-12-03 23:27:45 -0600

    12/3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/swarm/SwarmAutoBlueFront.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/swarm/SwarmAutoBlueFront.java
@@ -106,10 +106,10 @@ public class SwarmAutoBlueFront extends LinearOpMode {
 
     // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
     // this is only used for Android Studio when using models in Assets.
-    private static final String TFOD_MODEL_ASSET = "MyModelStoredAsAsset.tflite";
+    private static final String TFOD_MODEL_ASSET = "model_20231203_232252.tflite";
     // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
     // this is used when uploading models directly to the RC using the model upload interface.
-    //private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
+    //private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/model_20231203_232252.tflite";
     // Define the labels recognized in the model for TFOD (must be in training order!)
     private static final String[] LABELS = {
             "Blue",
